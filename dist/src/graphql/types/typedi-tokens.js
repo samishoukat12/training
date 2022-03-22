3b1468cf40577f7a394ce5291a28d57a4e734ed4
Muhammad Sami Shoukat <88966319+samishoukat12@users.noreply.github.com>
2022-03-22 23:13:23 +0500

    ts changes

--- dist/src/graphql/types/typedi-tokens.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prismaToken = void 0;
-var typedi_1 = require("typedi");
-exports.prismaToken = new typedi_1.Token('PrismaClient');
-//# sourceMappingURL=typedi-tokens.js.map
